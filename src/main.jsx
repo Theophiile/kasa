50b5b70b62608adb5224d7f67004703274ddbb2a
Théophile Pinto <theo-pinto@hotmail.fr>
2025-04-05 10:14:10 +0200

    ajustements

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/kasa">
+    <BrowserRouter>
       <AppRouter />
     </BrowserRouter>
   </React.StrictMode>
