64e72e36516325daf0e6de5ecdd2b08969c57f62
Théophile Pinto <theo-pinto@hotmail.fr>
2025-04-05 09:48:07 +0200

    ajout basename pour deployer

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/kasa">
       <AppRouter />
     </BrowserRouter>
   </React.StrictMode>
